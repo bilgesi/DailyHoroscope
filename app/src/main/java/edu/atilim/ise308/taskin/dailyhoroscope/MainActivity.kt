9111b5ea02b786b3fedd54bc2a8aa93d4f2b1d3f
bilgesi <72819994+bilgesi@users.noreply.github.com>
2020-10-29 20:34:25 +0300

    Initial commit

---
+++ app/src/main/java/edu/atilim/ise308/taskin/dailyhoroscope/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.atilim.ise308.taskin.dailyhoroscope
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_layout)
+    }
+}
